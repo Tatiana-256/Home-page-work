860b54b39c1c65fd62d4316bb37c9e6600d691ca
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-03-08 14:10:10 +0200

    Styled message.component

--- src/components/header/head.component.jsx
+++ src/components/header/head.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import styles from './head.module.css';
-import {avatar} from '../../common/header.JPG'
+import avatar from '../../common/header.JPG'
 
 
 const Header = () => {
     return (<div className={styles.head}>
         <div className={styles.ava}><img src={avatar} alt='photo'/></div>
-        <div>Матвиенко Татьяна Юрьевна</div>
+        <h2>Матвиенко Татьяна Юрьевна</h2>
     </div>)
 }
 
--- src/components/message/message.component.jsx
+++ src/components/message/message.component.jsx
@@ -3,8 +3,9 @@ import styles from './message.module.css';
 
 
 const Message = () => {
+
     return <div className={styles.main}>
-        <div>Well-motivated Junior Front-end Developer looking for an opportunity to implement my skills in JavaScript
+        <div className={styles.text}>Well-motivated Junior Front-end Developer looking for an opportunity to implement my skills in JavaScript
             and React. I have knowledge in JavaScript ES6 and ES5. Also I have a practice with Git
         </div>
     </div>
