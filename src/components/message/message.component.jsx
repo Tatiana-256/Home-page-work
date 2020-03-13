1f0d89f9b7e5a20772fa19474072a642c5f593b6
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-03-13 13:24:30 +0200

    Change my characteristics position and stiles

--- src/components/message/message.component.jsx
+++ src/components/message/message.component.jsx
@@ -9,18 +9,25 @@ const Message = () => {
         {character: "responsible", id: 1},
         {character: "friendly", id: 2},
         {character: "active", id: 3}]
-    // let classForCharacter = character[1] ? '' : "styles.bold"
+
 
     let today = new Date();
     let time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
     let date = `${today.getFullYear()}/${(today.getMonth() + 1)}/${today.getDate()}`;
 
     return <div className={styles.general}>
-        <div className={styles.box}> <img src={avatar} alt='photo'/>
-            <div>My characteristics</div>
+        <div className={styles.box}><img src={avatar} alt='photo'/>
+            <h2>My characteristics</h2>
             <div>
                 {
-                    characteristics.map(a => (<div className={styles.bold} key={a.id}>{a.character}</div>))
+                    characteristics.map(a => {
+                            if (a.id == 2) {
+                                return <div className={styles.bold} key={a.id}>{a.character}</div>
+                            } else {
+                                return <div key={a.id}>{a.character}</div>
+                            }
+                        }
+                    )
                 }
             </div>
         </div>
@@ -28,7 +35,7 @@ const Message = () => {
             <div>
                 <a>Матвиенко Татьяна Юрьевна</a>
             </div>
-            <textarea className={styles.text} placeholder='Your massage'></textarea>
+            <textarea className={styles.text} placeholder='Your message'></textarea>
             <div className={styles.time}>
                 <div>{date}</div>
                 <div>{time}</div>
