161660da208084115f607dc1e0337add8ab7b8b8
Tatiana-256 <tatiana.beznosiuk@gmail.com>
2020-03-27 15:45:20 +0200

    Updates

---
+++ precache-manifest.be1da275b2485a9a27d482496601282e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f91cf1a0e92ff4ba0cf1204649c2fa2c",
+    "url": "/Home-page-work/index.html"
+  },
+  {
+    "revision": "f82c7939f35061961517",
+    "url": "/Home-page-work/static/css/main.a90469a9.chunk.css"
+  },
+  {
+    "revision": "63f47e8d647d920f0596",
+    "url": "/Home-page-work/static/js/2.710106f5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Home-page-work/static/js/2.710106f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82c7939f35061961517",
+    "url": "/Home-page-work/static/js/main.233832c0.chunk.js"
+  },
+  {
+    "revision": "ee0af47f9fe4d0d0b836",
+    "url": "/Home-page-work/static/js/runtime-main.2d99402f.js"
+  },
+  {
+    "revision": "01718e6b2ec48dbac83622d87f514d4e",
+    "url": "/Home-page-work/static/media/header.01718e6b.JPG"
+  }
+]);
